c5ebfc3e7978983e8beb431ae3380eb8f8c1f3d3
MelvinMbae <melvinmuthomi95@gmail.com>
2023-11-20 15:51:35 +0300

    enclosure tables

--- server/app.py
+++ server/app.py
@@ -22,7 +22,7 @@ def animal_by_id(id):
     animal=Animal.query.filter(Animal.id==id).first()
     
     if not animal:
-        return '<404 Animal Not Found>'
+        return '<h1>404 Animal Not Found</h1>'
     
     return f'''
     <ul>ID:{animal.id}</ul>
@@ -36,7 +36,7 @@ def animal_by_id(id):
 def zookeeper_by_id(id):
     zookeeper=Zookeeper.query.filter(Zookeeper.id==id).first()
     if not zookeeper:
-        return '<404 Zookeeper Not Found>'
+        return '<h1>404 Zookeeper Not Found</h1>'
     
     response_body= f'''
         <ul>ID:{zookeeper.id}</ul>
@@ -52,7 +52,21 @@ def zookeeper_by_id(id):
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure=Enclosure.query.filter(Enclosure.id==id).first()
+    
+    if not enclosure:
+        return '<h1>404 Enclosure Not Found</h1>'
+    
+    response_body=f'''
+    <ul>Environment:{enclosure.environment}</ul>
+    <ul>Open to Visitors:{enclosure.open_to_visitors}</ul>
+    '''
+    
+    for animal in enclosure.animals:
+        response_body += f"<ul>Animal:{animal.name}</ul>"
+    
+    response=make_response(response_body)
+    return response
 
 
 if __name__ == '__main__':
